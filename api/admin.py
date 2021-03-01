877cab437e95ab24686790ab9d963e59867b9cd5
Boyko Alexey <elorian@yandex.ru>
2021-03-01 12:48:32 +0300

    admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Category, Genre, Title, Review, Comment
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Title)
+admin.site.register(Review)
+admin.site.register(Comment)
