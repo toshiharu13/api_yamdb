656be030c9f06676a20e2e2ab220c0573d1a2aa1
Boyko Alexey <elorian@yandex.ru>
2021-03-01 16:43:38 +0300

    delete auth app

--- authorize/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class PreUser(models.Model):
-    email = models.CharField(max_length=50)
-    confirmation_code = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.email
--- authorize/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import TokenSend, mail_send
-
-urlpatterns = [
-    path('email/', mail_send),
-    path('token/', TokenSend),
-]
