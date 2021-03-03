d76e7ed7d2cf075b1dae74c0c72723d01f3629c3
DarkhanZhumashev <darkhanzhumashev@gmail.com>
2021-03-03 14:46:50 +0600

    final version 2.0

---
+++ api/validators.py
@@ -0,0 +1,11 @@
+
+from django.core.validators import ValidationError
+from django.utils import timezone
+
+
+def year_validator(value):
+    if value > timezone.now().year:
+        raise ValidationError(
+            'Введите корректный год!',
+            params={'value': value},
+        )
